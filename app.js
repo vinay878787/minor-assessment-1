b9f58d73274b0aabba07cb87c8bc3f953f1c3507
B H Vinay <76247444+vinay878787@users.noreply.github.com>
2022-12-09 22:40:31 +0530

    Boiler plate setup
    boilerplate setup for app.js of minor assessment

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000 ;
+
+app.listen(port, function(){
+   console.log("server is running @ ${port}");
+});
