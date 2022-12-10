b7f79437c38b9a001f05c4d9965bf3cb807b080c
B H Vinay <76247444+vinay878787@users.noreply.github.com>
2022-12-10 17:23:19 +0530

    added database config and initialzation
    added database config and initialzation

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 require('dotenv').config()
+const mongoose = require("mongoose");
 const express = require("express");
+var Schema = mongoose.Schema ;
 
 const app = express();
 
-const port = process.env.PORT || 3000 ;
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+mongoose.set('strictQuery', true);
 
-app.listen(port, function(){
-   console.log("server is running @ ${port}");
+
+const PORT = process.env.PORT || 3000 ;
+const HOST = process.env.HOST || "localhost" ;
+
+
+
+
+app.get("/api/health",function(req,res){
+   res.send(`Backend server is active status : active and time ${new Date()}`)
+})
+app.listen(PORT, function(){
+   console.log(`http://${HOST}:${PORT}`);
 });
